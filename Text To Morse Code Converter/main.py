3ef72edee259a79ba9473a0d1d142f15c3114faf
Joshua Lehr <joshua.valian17@gmail.com>
2023-09-06 17:40:42 -0600

    Fix continue request loop

--- Text To Morse Code Converter/main.py
+++ Text To Morse Code Converter/main.py
@@ -1,19 +1,19 @@
-
+# ----- Variable Setup ----- #
 morse_dict = {'a': '·−', 'b': '−···', 'c': '−·−·', 'd': '−··', 'e': '·', 'f': '··−·', 'g': '−−·', 'h': '····',
               'i': '··', 'j': '·−−−', 'k': '−·−', 'l': '·−··', 'm': '−−', 'n': '−·', 'o': '−−−', 'p': '·−−·',
               'q': '−−·−', 'r': '·−·', 's': '···', 't': '−', 'u': '··−', 'v': '···−', 'w': '·−−', 'x': '−··−',
               'y': '−·−−', 'z': '−−··', '0': '−−−−−', '1': '·−−−−', '2': '··−−−', '3': '···−−', '4': '····−',
               '5': '·····', '6': '−····', '7': '−−···', '8': '−−−··', '9': '−−−−·', ' ': ' '}
-
-
-print('Welcome to the Morse Code Text Converter!')
-to_translate = input('Please type your message here (letters, numbers and spaces only): ').lower()
-
 playing = True
+
+# ----- Morse Code Translator ----- #
 while playing:
+    answering = True
+    print('Welcome to the Morse Code Text Converter!')
+    to_translate = input('Please type your message here (letters, numbers and spaces only): ').lower()
+
     translatable = False
     while not translatable:
-        translatable = False
         for char in to_translate:
             if char not in morse_dict:
                 translatable = False
@@ -25,18 +25,25 @@ while playing:
                                      f"Please try again (letters, numbers and spaces only): ").lower()
                 break
 
+    # --- Converting text to morse code --- #
     translated = [morse_dict[char] for char in to_translate]
     morse_message = ""
-
     for char in translated:
         morse_message += char
 
+    # --- Output converted message --- #
     print(f"Your message in morse code is {morse_message}")
     answer = input("Would you like to encode another message? Y/N: ").upper()
 
-    if answer == "N":
-        playing = False
-    elif answer == "Y":
-        playing = True
-    else:
+    # --- Ask user if they want to continue --- #
+    while answering:
+        if answer == "N":
+            playing = False
+            answering = False
+        elif answer == "Y":
+            playing = True
+            answering = False
+        else:
+            answer = input("Incorrect input. Would you like to encode another message? Y/N: ").upper()
 
+print("Thank you for using our Morse Code Text Converter!")
