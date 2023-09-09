910c8ac1bdd5a6d4a7cf4ce9381c039879878cf7
Joshua Lehr <joshua.valian17@gmail.com>
2023-09-09 10:35:44 -0600

    Add computer's turn method and update board method.

--- Tic Tac Toe/game_master.py
+++ Tic Tac Toe/game_master.py
@@ -1,11 +1,39 @@
+from random import randint
+
+
 class TicTacToe:
 
     def __init__(self):
         self.positions = [" " for _ in range(0, 9)]
-        self.game_board = f" ___________\n" \
+        self.game_board = f" ___________\n"\
+                          f"| {self.positions[0]} | {self.positions[1]} | {self.positions[2]} |\n" \
+                          f"|___|___|___|\n" \
+                          f"| {self.positions[3]} | {self.positions[4]} | {self.positions[5]} |\n" \
+                          f"|___|___|___|\n" \
+                          f"| {self.positions[6]} | {self.positions[7]} | {self.positions[8]} |\n" \
+                          f"|___|___|___|\n"
+        self.chosen_pos = 0
+
+    def player_move(self):
+        pass
+
+    def comp_move(self):
+        self.chosen_pos = randint(0, len(self.positions) - 1)
+        can_move = False
+        while not can_move:
+            if self.positions[self.chosen_pos] != " ":
+                self.chosen_pos = randint(0, len(self.positions) - 1)
+            else:
+                can_move = True
+        self.positions[self.chosen_pos] = "O"
+        self.update_board()
+
+    def update_board(self):
+        self.game_board = f" ___________\n"\
                           f"| {self.positions[0]} | {self.positions[1]} | {self.positions[2]} |\n" \
                           f"|___|___|___|\n" \
                           f"| {self.positions[3]} | {self.positions[4]} | {self.positions[5]} |\n" \
                           f"|___|___|___|\n" \
                           f"| {self.positions[6]} | {self.positions[7]} | {self.positions[8]} |\n" \
                           f"|___|___|___|\n" \
+
--- Tic Tac Toe/main.py
+++ Tic Tac Toe/main.py
@@ -3,3 +3,5 @@ import game_master as gm
 new_game = gm.TicTacToe()
 
 print(new_game.game_board)
+new_game.comp_move()
+print(new_game.game_board)
