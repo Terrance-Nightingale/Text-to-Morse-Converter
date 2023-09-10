d0ad90bc5edf570f7b6670f7d38ed4655a97c9d2
Joshua Lehr <joshua.valian17@gmail.com>
2023-09-10 11:41:33 -0600

    Revert "Add method to play again."
    This reverts commit d6e00ae61804fd65a3b483502a2ff72f17bf8a96.

--- Tic Tac Toe/game_master.py
+++ Tic Tac Toe/game_master.py
@@ -5,31 +5,16 @@ from time import sleep
 class TicTacToe:
 
     def __init__(self):
-        self.playing = True
         self.positions = [(_ + 1) for _ in range(0, 9)]
         self.approved_moves = [_ for _ in range(0, 9)]
+        self.win_con = [3, 9, 12, 15, 21]
+        self.board_state = [[]]
         self.game_board = f" ___________\n"
         self.update_board()
         print("Welcome to Tic Tac Toe!")
 
     def play_game(self):
-        while self.playing:
-            self.player_move()
-            self.check_winner()
-            if not self.playing:
-                break
-
-            self.comp_move()
-            self.check_winner()
-            if not self.playing:
-                break
-
-    def play_again(self):
-        choice = input("Play again? Y/N: ").upper()
-        if choice == "Y":
-            return True
-        elif choice == "N":
-            return False
+        pass
 
     def player_move(self):
         can_move = False
@@ -74,52 +59,9 @@ class TicTacToe:
 
     def update_board(self):
         rows = 3
-        self.board_state = [[self.positions[_] for _ in range(0,3)],
-                            [self.positions[_] for _ in range(3,6)],
-                            [self.positions[_] for _ in range(6,9)]]
         self.game_board = f" ___________\n"
         for _ in range(0, rows):
             count = _ * 3
             self.game_board += f"| {self.positions[count]} | {self.positions[count+1]} | {self.positions[count+2]} |\n" \
                                f"|___|___|___|\n"
-        print(self.board_state)
         print(self.game_board)
-
-    def check_winner(self):
-        player_score = 0
-        comp_score = 0
-        player_win = False
-        comp_win = False
-        #--- Check columns ---#
-        for row in self.board_state:
-            if row[0] == "X":
-                player_score += 1
-            elif row[0] == "O":
-                comp_score += 1
-            else:
-                pass
-
-        #--- Check rows ---#
-        for row in self.board_state:
-            for symbol in row:
-                if symbol == "X":
-                    player_score += 1
-                elif symbol == "O":
-                    comp_score += 1
-                else:
-                    pass
-
-        #--- Check diagonals ---#
-        # starting_pos = 0
-        # for row in self.board_state:
-        #     row[starting_pos]
-
-        #--- Return winner if any ---#
-        if player_score == 3:
-            print("Congratulations! You win!")
-            self.play_again()
-        elif comp_score == 3:
-            print("You lose.")
-            self.play_again()
-        else:
-            pass
