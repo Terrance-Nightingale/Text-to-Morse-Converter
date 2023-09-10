6f4c72bd1d5634a8a3ca5c4517afcc3df0bd1ba7
Joshua Lehr <joshua.valian17@gmail.com>
2023-09-10 11:41:33 -0600

    Revert "Score by rows and columns, declare winner, and allow to play again."
    This reverts commit a7a600574757c57c1b97b69a9f671cf45bb1b41e.

--- Tic Tac Toe/game_master.py
+++ Tic Tac Toe/game_master.py
@@ -5,51 +5,55 @@ from time import sleep
 class TicTacToe:
 
     def __init__(self):
-        self.player_score = 0
-        self.comp_score = 0
         self.playing = True
-        self.positions = []
-        self.approved_moves = [_ for _ in range(1, 10)]
-        self.board_state = []
-        self.game_board = None
-
-    def play_game(self):
         self.positions = [(_ + 1) for _ in range(0, 9)]
+        self.approved_moves = [_ for _ in range(0, 9)]
+        self.game_board = f" ___________\n"
         self.update_board()
         print("Welcome to Tic Tac Toe!")
+
+    def play_game(self):
         while self.playing:
             self.player_move()
             self.check_winner()
             if not self.playing:
-                return
+                break
 
             self.comp_move()
             self.check_winner()
             if not self.playing:
-                return
+                break
+
+    def play_again(self):
+        choice = input("Play again? Y/N: ").upper()
+        if choice == "Y":
+            return True
+        elif choice == "N":
+            return False
 
     def player_move(self):
         can_move = False
         try:
             chosen_pos = int(input("Please make your move by typing a number from 1-9 "
                                    "(whole numbers only): ")) - 1
-            if chosen_pos not in range(0, 9) or self.positions[chosen_pos] not in self.approved_moves:
-                raise ValueError
-            else:
+            if chosen_pos in range(0, 9):
                 can_move = True
                 self.positions[chosen_pos] = "X"
                 self.update_board()
+            else:
+                raise ValueError
         except ValueError:
             while not can_move:
                 try:
                     chosen_pos = int(input("Incorrect input. Please make your move by typing a number from 1-9 "
                                            "(whole numbers only): ")) - 1
-                    if chosen_pos not in range(0, 9) or self.positions[chosen_pos] not in self.approved_moves:
-                        raise ValueError
-                    else:
+                    if chosen_pos in range(0, 9):
                         can_move = True
+                        print(chosen_pos)
                         self.positions[chosen_pos] = "X"
                         self.update_board()
+                    else:
+                        raise ValueError
                 except ValueError:
                     pass
 
@@ -57,12 +61,12 @@ class TicTacToe:
         print("Computer is thinking...")
         sleep(2)
         chosen_pos = randint(0, len(self.positions) - 1)
-        print(f"Computer's move: Position {chosen_pos + 1}")
+        print(f"Computer's move: Position {chosen_pos}")
         can_move = False
         while not can_move:
             if self.positions[chosen_pos] not in self.approved_moves:
                 chosen_pos = randint(0, len(self.positions) - 1)
-                print(f"Chosen comp position now = {chosen_pos + 1}")
+                print(f"Chosen comp position now = {chosen_pos}")
             else:
                 can_move = True
         self.positions[chosen_pos] = "O"
@@ -78,62 +82,44 @@ class TicTacToe:
             count = _ * 3
             self.game_board += f"| {self.positions[count]} | {self.positions[count+1]} | {self.positions[count+2]} |\n" \
                                f"|___|___|___|\n"
+        print(self.board_state)
         print(self.game_board)
 
     def check_winner(self):
+        player_score = 0
+        comp_score = 0
+        player_win = False
+        comp_win = False
         #--- Check columns ---#
-        self.player_score = 0
-        self.comp_score = 0
         for row in self.board_state:
             if row[0] == "X":
-                self.player_score += 1
+                player_score += 1
             elif row[0] == "O":
-                self.comp_score += 1
+                comp_score += 1
             else:
-                continue
-        if self.player_score == 3 or self.comp_score == 3:
-            self.playing = False
-            self.declare_winner()
-            return
+                pass
 
         #--- Check rows ---#
-        self.player_score = 0
-        self.comp_score = 0
         for row in self.board_state:
             for symbol in row:
                 if symbol == "X":
-                    self.player_score += 1
+                    player_score += 1
                 elif symbol == "O":
-                    self.comp_score += 1
+                    comp_score += 1
                 else:
-                    continue
-        if self.player_score == 3 or self.comp_score == 3:
-            self.playing = False
-            self.declare_winner()
-            return
+                    pass
 
         #--- Check diagonals ---#
         # starting_pos = 0
         # for row in self.board_state:
         #     row[starting_pos]
 
-    def declare_winner(self):
         #--- Return winner if any ---#
-        if self.player_score == 3:
+        if player_score == 3:
             print("Congratulations! You win!")
             self.play_again()
-        elif self.comp_score == 3:
+        elif comp_score == 3:
             print("You lose.")
             self.play_again()
-
-    def play_again(self):
-        legal_choices = ("Y", "N")
-        choice = input("Play again? Y/N: ").upper()
-        while choice not in legal_choices:
-            choice = input("Incorrect input. Play again? Y/N: ").upper()
-        if choice == "Y":
-            self.playing = True
-            self.play_game()
-        elif choice == "N":
-            print("Thank you for playing!")
-            self.playing = False
+        else:
+            pass
--- Tic Tac Toe/main.py
+++ Tic Tac Toe/main.py
@@ -2,4 +2,5 @@ from game_master import TicTacToe
 
 new_game = TicTacToe()
 
-new_game.play_game()
+new_game.player_move()
+new_game.comp_move()
