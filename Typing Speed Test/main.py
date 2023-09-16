b9158459e3c667ba53396264fc0c6d0a427c3573
Joshua Lehr <joshua.valian17@gmail.com>
2023-09-16 11:50:22 -0600

    Add sample text for typing.

--- Typing Speed Test/main.py
+++ Typing Speed Test/main.py
@@ -3,6 +3,18 @@ import time
 import math
 
 timer = None
+sample = "\"Studying is the main source of knowledge. Books are indeed never failing friends of man. " \
+         "For a mature mind, reading is the greatest source of pleasure and solace to distressed minds. " \
+         "The study of good books ennobles us and broadens our outlook. Therefore," \
+         " the habit of reading should be cultivated. A student should never confine himself to his schoolbooks only." \
+         " He should not miss the pleasure locked in the classics, poetry, drama, history, philosophy etc." \
+         " We can derive benefit from other’s experiences with the help of books. " \
+         "The various sufferings, endurance and joy described in books enable us to have a closer look at human" \
+         " life. They also inspire us to face the hardships of life courageously. " \
+         "Nowadays there are innumerable books and time is scarce. " \
+         "So we should read only the best and the greatest among them. " \
+         "With the help of books we shall be able to make our thinking mature " \
+         "and our life more meaningful and worthwhile.\""
 
 
 def start_timer():
@@ -29,12 +41,12 @@ window.title("Typing Speed Test App")
 window.minsize(width=600, height=500)
 window.config(padx=50, pady=20)
 
-# Insert title label
-# Insert function for
-
-title_label = Label(text="Test your typing speed: ", font=("Ariel", 20, "bold"))
+title_label = Label(text="Test your typing speed! Type the following within 1 minute: ", font=("Ariel", 15, "bold"))
 title_label.pack()
-title_label.config(justify="left", anchor="s")
+
+sample_label = Label(text=sample, font=("Ariel", 10, "bold"), highlightthickness=5, borderwidth=3, relief="solid")
+sample_label.config(wraplength=500, anchor="e", justify="left", pady=10, padx=10)
+sample_label.pack()
 
 test_entry = Text(width=50, height=15)
 test_entry.pack()
@@ -42,7 +54,7 @@ test_entry.focus()
 
 canvas = Canvas(width=200, height=120, highlightthickness=0)
 timer_title = canvas.create_text(100, 50, text="Timer", font=("Courier", 24, "bold"))
-timer_text = canvas.create_text(100, 100, text="00:00", font=("Courier", 35, "bold"))
+timer_text = canvas.create_text(100, 100, text="1:00", font=("Courier", 35, "bold"))
 canvas.pack()
 
 start_btn = Button(text="Start", font=("Courier", 24, "bold"), command=start_timer)
